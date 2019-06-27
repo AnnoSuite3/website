438d5c759deec056a45668b82f68b48fc862d13d
MNTN <mntn@MNTNs-iMac.local>
2019-06-27 12:40:38 -0700

    build(Index): Includes message, and estimated launch date
    No Styling

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Head from "next/head";
+import { Container } from "../components";
+
+export default () => (
+  <Container>
+    <Head>
+      <title>AnnoSuite</title>
+    </Head>
+    <section>
+      <h1>AnnoSuite</h1>
+      <h2>Coming</h2>
+      <h2>This Winter!</h2>
+      <p>Digital life revolutionized</p>
+    </section>
+  </Container>
+);
