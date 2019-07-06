38193aaf446e0a672bbe49aa3edb90356e864653
MNTN <mntn@MNTNs-iMac.local>
2019-07-05 22:30:04 -0700

    fix(linaria with ssr): Implemented according to example
    Fixes issue where styles would not be applied to elements

--- next.config.js
+++ next.config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.js$/,
+      use: [
+        {
+          loader: "linaria/loader",
+          options: {
+            sourceMap: process.env.NODE_ENV !== "production"
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
+});
